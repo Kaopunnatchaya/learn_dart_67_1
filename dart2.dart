15c35803cde8843d7349830366116b55fe5559fc
Kaopunnatchaya <boonnak9871@gmail.com>
2025-07-26 14:48:06 +0700

    Step 2: Import File

---
+++ dart2.dart
@@ -0,0 +1,11 @@
+// Import File
+import "dart2a.dart";
+
+void main() {
+  // Accessing the public key
+  print("Public Key: $publicKey");
+
+  // Attempting to access the private key will result in an error
+  // Uncommenting the next line will cause a compilation error
+  // print("Private Key: $_secretKey");
+}
