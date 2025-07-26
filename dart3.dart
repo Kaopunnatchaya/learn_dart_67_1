7f904f9f6f10f1c400e56bb50059f8a7465a30a0
Kaopunnatchaya <boonnak9871@gmail.com>
2025-07-26 15:04:41 +0700

    Step 3: Startic Variable Type

---
+++ dart3.dart
@@ -0,0 +1,24 @@
+// Startic Variable Type
+void main() {
+  // Senstive Case
+  String name = "Alice";
+  String _name = "Bob"; 
+  String nAme = "Charlie"; 
+
+  print(name);
+  print(_name);
+  print(nAme);
+  print("------------------------");
+
+  name = "David";
+  int age = 20;
+  double score =95.5;
+  num height = 175.5, weight = 70;
+  bool isStudet = true;
+  print("ชื่อ : $name");
+  print("อายุ : $age");
+  print("คะแนน : $score");
+  print("ส่วนสูง : $height ซม."); 
+  print("น้ำหนัก : $weight กก.");
+  print("นักเรียน : $isStudet");
+}
